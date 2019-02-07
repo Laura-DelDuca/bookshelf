6efda86c848e375c0a874cee292b901e3eb0e7c2
benestruv <benedicte.struvay@gmail.com>
2019-02-07 16:03:29 +0100

    Liste books

--- src/client/components/Library_Availablebooks.js
+++ src/client/components/Library_Availablebooks.js
@@ -1,14 +1,20 @@
-// import * as React from "react";
-// import {books} from "./databasetest";
+import * as React from "react";
+import {books} from "./databasetest";
 
-// export default class listBooks extends React.Component {
-//     render() {
-//         const BooksList = books.map(book => {
-//             return (
-//                 <li key={book.id} className="book">
-//                     <h3>{book.title}</h3>
-//                 </li>
-//             );
-//         });
-//     }
-// }
+export default class listBooks extends React.Component {
+    render() {
+        const BookList = books.map(book => {
+            return (
+                <li key={book.id}>
+                    <h3>{book.title}</h3>
+                </li>
+            );
+        });
+
+        return (
+            <div>
+                <div>{BookList}</div>
+            </div>
+        );
+    }
+}
