63289230ef53d3e2e761c8dfac5bde65fe6cd68a
Laura Del Duca <laura.delduca1990@gmail.com>
2019-02-12 10:27:42 +0100

    commit before merge

--- src/client/components/Library.js
+++ src/client/components/Library.js
@@ -6,7 +6,7 @@ export default class Library extends React.Component {
     render() {
         return (
             <div className="container_library">
-                <div className="divnavbar">
+                <div>
                     <NavBar />
                 </div>
                 <div className="titleavailable_library">
